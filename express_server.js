31f182c24b6cb87f3ab1558f3f40c7925d128349
Kevin Chu <kvnchu2@gmail.com>
2020-12-07 23:12:13 +0000

    added in ejs

--- express_server.js
+++ express_server.js
@@ -1,6 +1,8 @@
+app.set("view engine", "ejs");
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
