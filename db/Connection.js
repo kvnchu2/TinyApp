91d47cd7dff94070fc758d594c4368509f84718a
Kevin Chu <kvnchu2@gmail.com>
2021-03-07 18:08:12 -0800

    add env variable for connection uri

--- db/Connection.js
+++ db/Connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const URI = 'mongodb+srv://kchu:123@cluster0.nqugv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const URI = process.env.DB_CONNECTION;
 
 const connectDB = async() => {
   await mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
