def833e18e0bf580b8220d6b8c77b0211199bc56
Kevin Chu <kvnchu2@gmail.com>
2021-03-07 17:08:14 -0800

    delete bcrypt

--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { validate, getUserByEmail, users } = require('../helpers.js');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 module.exports = () => {
--- routes/register.js
+++ routes/register.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const { validate, generateRandomString, createObj, urlDatabase } = require('../helpers.js');
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 const User = require('../models/User');
 
 
